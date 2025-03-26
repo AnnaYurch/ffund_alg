9656680db7725f553e8ec1e65b8e4d6e077c0568
AnnaYurch <anna.yurch2005@gmail.com>
2025-03-26 23:53:01 +0300

    1.7

---
+++ sis_prog/1/7/main.c
@@ -0,0 +1,66 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+typedef enum Errors {
+    INVALID_MEMORY,
+    INVALID_ARG,
+    INVALID_FILE,
+    INVALID_DATA,
+    INVALID_LIMIT,
+    INVALID_WRITTEN,
+    INVALID_READ,
+    INVALID_PATH,
+    INVALID_SYSCALL,
+    OK
+} Errors;
+
+Errors list_directory(const char *path) {
+    int fd = open(path, O_RDONLY | O_DIRECTORY); //только для чтения и только для каталогов
+    if (fd == -1) {
+        return INVALID_PATH;
+    }
+
+    char buffer[1024];
+    ssize_t bytes_read; // -1
+
+    while ((bytes_read = syscall(SYS_getdents64, fd, buffer, sizeof(buffer))) > 0) {
+        for (char *ptr = buffer; ptr < buffer + bytes_read; ptr += *(unsigned short *)(ptr + 16)) { //de_reclen
+            char *d_name = ptr + 19;
+            if (*d_name != '.') {    
+                printf("   %s\n", d_name);
+            }
+        }
+    }
+
+    if (bytes_read == -1) {
+        close(fd);
+        return INVALID_SYSCALL;
+    }
+
+    close(fd);
+    return OK;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Write: %s <directory> ...? <directory>\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        printf("Directory: %s\n", argv[i]);
+        Errors err = list_directory(argv[i]);
+        if (err == INVALID_PATH) {
+            printf("   INVALID_PATH\n");
+        } else if(err == INVALID_SYSCALL) {
+            printf("   INVALID_SYSCALL\n");
+        }
+    }
+
+    return 0;
+}
